25150e242cea0cf85e3d08d92414faf1c9eb696c
Botyto <biserkrustev.botyto@gmail.com>
2018-11-09 19:20:55 +0200

    Implement foundation for new dice value selection logic

---
+++ Assets/Code/DiceValueSelector.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class DiceValueSelector : MonoBehaviour
+{
+    public enum DiceValue
+    {
+        Undefined = 0,
+        One = 1,
+        Two = 2,
+        Three = 3,
+        Four = 4,
+        Five = 5,
+        Six = 6,
+    }
+
+    public DiceValue upValue;
+    public DiceValue forwardValue;
+    public DiceValue rightValue;
+
+    private Rigidbody m_Rigidbody;
+
+    public DiceValue SelectedValue()
+    {
+        return DiceValue.Undefined; //TODO implement me
+    }
+
+    public IEnumerator WaitToSettle()
+    {
+        while (m_Rigidbody.IsSleeping())
+        {
+            yield return null;
+        }
+    }
+
+    void OnEnable()
+    {
+        m_Rigidbody = GetComponent<Rigidbody>();
+    }
+}
