bdfb9c50b98135d14a83ae749073f492c5cc3c5a
David Parushev <davidn.1107@gmail.com>
2018-11-13 18:12:06 +0200

    dice fixes

---
+++ Assets/Code/Dice/Dice.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dice : MonoBehaviour
+{
+    private int currentValue = 1;
+    public bool rollComplete = true;
+
+    public float forceAmount = 40.0f;
+    public float torqueAmount = 150.0f;
+    public float rotateDuration = 4f;
+    public ForceMode forceMode;
+
+    public Rigidbody rigidbody;
+    public DiceValueSelector diceValueSelector;
+
+    private void Awake()
+    {
+        diceValueSelector = GetComponent<DiceValueSelector>();
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    public IEnumerator ShakeDice()
+    {
+        rollComplete = false;
+        this.transform.position += new Vector3(0, 7, 0);
+        rigidbody.AddForce(Random.onUnitSphere * forceAmount, forceMode);
+        rigidbody.AddTorque(Random.onUnitSphere * torqueAmount, forceMode);
+        yield return Rotate();
+
+        while (!GetComponent<Rigidbody>().IsSleeping())
+        {
+            yield return null;
+        }
+
+        if(rigidbody.IsSleeping())
+        currentValue = (int)diceValueSelector.SelectedValue();
+        GameController.instance.DiceSum += currentValue;
+        Debug.Log("Die roll complete, " + currentValue);
+        rollComplete = true;
+    }
+
+    IEnumerator Rotate()
+    {
+        float startRotation = transform.eulerAngles.y;
+        float endRotation = startRotation + Random.Range(10f, 360.0f);
+        float t = 0.0f;
+        while (t < rotateDuration)
+        {
+            t += Time.deltaTime;
+            float yRotation = Mathf.Lerp(startRotation, endRotation, t / rotateDuration) % 360.0f;
+            transform.eulerAngles = new Vector3(transform.eulerAngles.x, yRotation, transform.eulerAngles.z);
+            yield return null;
+        }
+    }
+
+    //void Awake()
+    //{
+    //    // For the sake of this example we assume a regular cube dice if 
+    //    // directions haven't been specified in the editor. Sum of opposite
+    //    // sides is 7, haven't consider exact real layout though.
+    //    if (directions.Count == 0)
+    //    {
+    //        // Object space directions
+    //        directions.Add(Vector3.up);
+    //        sideValues.Add(4); // up
+    //        directions.Add(Vector3.down);
+    //        sideValues.Add(3); // down
+
+    //        directions.Add(Vector3.left);
+    //        sideValues.Add(2); // left
+    //        directions.Add(Vector3.right);
+    //        sideValues.Add(5); // right
+
+    //        directions.Add(Vector3.forward);
+    //        sideValues.Add(6); // fw
+    //        directions.Add(Vector3.back);
+    //        sideValues.Add(1); // back
+    //    }
+
+    //    // Assert equal side of lists
+    //    if (directions.Count != sideValues.Count)
+    //    {
+    //        Debug.LogError("Not consistent list sizes");
+    //    }
+    //}
+
+    //// Gets the number of the side pointing in the same direction as the reference vector,
+    //// allowing epsilon degrees error.
+    //public int GetNumber(Vector3 referenceVectorUp, float epsilonDeg = 5f)
+    //{
+    //    // here I would assert lookup is not empty, epsilon is positive and larger than smallest possible float etc
+    //    // Transform reference up to object space
+    //    Vector3 referenceObjectSpace = transform.InverseTransformDirection(referenceVectorUp);
+
+    //    // Find smallest difference to object space direction
+    //    float min = float.MaxValue;
+    //    int mostSimilarDirectionIndex = -1;
+    //    for (int i = 0; i < directions.Count; ++i)
+    //    {
+    //        float a = Vector3.Angle(referenceObjectSpace, directions[i]);
+    //        if (a <= epsilonDeg && a < min)
+    //        {
+    //            min = a;
+    //            mostSimilarDirectionIndex = i;
+    //        }
+    //    }
+
+    //    // -1 as error code for not within bounds
+    //    return (mostSimilarDirectionIndex >= 0) ? sideValues[mostSimilarDirectionIndex] : -1;
+    //}
+}
---
+++ Assets/Code/Dice/DiceValueSelector.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class DiceValueSelector : MonoBehaviour
+{
+    private float epsilonDeg = 30f;
+    public List<Vector3> directions = new List<Vector3>{Vector3.up, Vector3.down, Vector3.left, Vector3.right, Vector3.back, Vector3.forward};
+
+    public enum DiceValue
+    {
+        Undefined = 0,
+        One = 1,
+        Two = 2,
+        Three = 3,
+        Four = 4,
+        Five = 5,
+        Six = 6,
+    }
+
+    public DiceValue upValue;
+    public DiceValue forwardValue;
+    public DiceValue rightValue;
+
+    private Rigidbody m_Rigidbody;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Debug.Log(SelectedValue().ToString());
+        }
+    }
+
+    public DiceValue SelectedValue()
+    {
+        Vector3 referenceObjectSpace = transform.InverseTransformDirection(Vector3.up);
+        // Find smallest difference to object space direction
+        float min = float.MaxValue;
+        int mostSimilarDirectionIndex = -1;
+        for (int i = 0; i < 6; i++)
+        {
+            float a = Vector3.Angle(referenceObjectSpace, directions[i]);
+            if (a <= epsilonDeg && a < min)
+            {
+                min = a;
+                mostSimilarDirectionIndex = i + 1;
+            }
+        }
+
+        switch (mostSimilarDirectionIndex)
+        {
+            case 1: return upValue;
+            case 2: return OppositeValue(upValue);
+            case 3: return rightValue;
+            case 4: return OppositeValue(rightValue);
+            case 5: return forwardValue; 
+            case 6: return OppositeValue(forwardValue); ;
+            default: return 0;         // 0 as error code for not within bounds
+        }
+    }
+
+    public IEnumerator WaitToSettle()
+    {
+        while (m_Rigidbody.IsSleeping())
+        {
+            yield return null;
+        }
+    }
+
+    private DiceValue OppositeValue(DiceValue value)
+    {
+        return (DiceValue)(DiceValue.Six - (value - 1));
+    }
+
+    void OnEnable()
+    {
+        m_Rigidbody = GetComponent<Rigidbody>();
+    }
+}
