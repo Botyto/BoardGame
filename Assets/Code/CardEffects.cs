9feaaba97aee8d0a0b1613358c2453b4ea002437
Botyto <biserkrustev.botyto@gmail.com>
2018-11-28 20:43:31 +0200

    Implement "get param" functions for card effects Fix "Choose Player" card effects

--- Assets/Code/CardEffects.cs
+++ Assets/Code/CardEffects.cs
@@ -1,17 +1,17 @@
 ﻿using System.Collections;
-using UnityEngine;
 
 public class CardEffects
 {
     public static IEnumerator ModifyTurns(CardDefinition definition, Card card, Player player)
     {
-        int count;
-        if (!int.TryParse(definition.GetParameter("turns", defaultValue: "1"), out count))
+        var playerIdx = GetIntParam(definition, card, "playerIdx", player.playerNumber);
+        var count = GetIntParam(definition, card, "turns", 1);
+
+        if (playerIdx >= 0 && playerIdx < GameController.instance.playersCount)
         {
-            count = 1;
+            GameController.instance.players[playerIdx].numberOfTurns += count;
         }
 
-        player.numberOfTurns += count;
         return null;
     }
 
@@ -23,9 +23,8 @@ public class CardEffects
 
     public static IEnumerator ChoosePlayer(CardDefinition definition, Card card, Player player)
     {
-        Debug.Log("choose player");
-        var text = definition.GetParameter("text", defaultValue: "Choose player");
-        var title = definition.GetParameter("title", defaultValue: "Choose player");
+        var text = GetStringParam(definition, card, "text", "Choose player");
+        var title = GetStringParam(definition, card, "title", "Choose player");
 
         var msgBox = Dialog.Spawn<MessageBox>();
         msgBox.titleControl.text = title;
@@ -33,17 +32,50 @@ public class CardEffects
         msgBox.hasCloseButton = false;
 
         var playersCount = GameController.instance.playersCount;
-        msgBox.additionalButtons = new MessageBox.ButtonEntry[playersCount];
+        var buttons = new MessageBox.ButtonEntry[playersCount];
+        msgBox.additionalButtons = buttons;
         for (int i = 0; i < playersCount; ++i)
         {
-            msgBox.additionalButtons[i].text = string.Format("Player {0}", i + 1);
-            msgBox.additionalButtons[i].onClick.AddListener(() => card.WriteData("playerIdx", i));
+            buttons[i].text = string.Format("Player {0}", i + 1);
+            buttons[i].onClick = new UnityEngine.UI.Button.ButtonClickedEvent();
+            buttons[i].onClick.AddListener(() => { card.WriteData("playerIdx", i); msgBox.Close(); });
+        }
+        
+        yield return new WaitForObjectDestroyed(msgBox.gameObject);
+    }
+
+    #region Parameter getters
+
+    private static string GetStringParam(CardDefinition definition, Card card, string key, string defaultValue = "")
+    {
+        var data = card.ReadData(key) as string;
+        if (!string.IsNullOrEmpty(data))
+        {
+            return data;
         }
 
-        Debug.Log("choose player 2");
-
-        yield return new WaitForObjectDestroyed(msgBox.gameObject);
-
-        Debug.Log("choose player 3");
+        return definition.GetParameter(key, defaultValue);
     }
+
+    private static int GetIntParam(CardDefinition definition, Card card, string key, int defaultValue = 0)
+    {
+        var data = card.ReadData(key);
+        if (data is int)
+        {
+            return (int)data;
+        }
+
+        var param = definition.GetParameter(key, defaultValue.ToString());
+        int result;
+        if (int.TryParse(param, out result))
+        {
+            return result;
+        }
+        else
+        {
+            return defaultValue;
+        }
+    }
+
+    #endregion
 }
