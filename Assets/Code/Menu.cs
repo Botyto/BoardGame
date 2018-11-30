60116ade4ede73883eb8460393b705f2b924915e
David Parushev <davidn.1107@gmail.com>
2018-11-30 14:11:30 +0200

    Simple Menu files :)

---
+++ Assets/Code/Menu.cs
@@ -0,0 +1,43 @@
+﻿
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour {
+
+    public Canvas canvasMenu;
+    public Canvas canvasOptions;
+    public Canvas canvasPlayerSelection;
+
+    public void ShowOptions()
+    {
+        canvasMenu.gameObject.SetActive(false);
+        canvasOptions.gameObject.SetActive(true);
+    }
+    public void HideOptions()
+    {
+        canvasOptions.gameObject.SetActive(false);
+        canvasMenu.gameObject.SetActive(true);
+    }
+
+    public void ShowPlayerSelection()
+    {
+        canvasMenu.gameObject.SetActive(false);
+        canvasPlayerSelection.gameObject.SetActive(true);
+    }
+    public void BackToMenuFromPlayersSelect()
+    {
+        canvasPlayerSelection.gameObject.SetActive(false);
+        canvasMenu.gameObject.SetActive(true);
+    }
+    
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Assets/Scenes/Board.unity");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+}
