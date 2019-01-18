334fa63c48d95e5466db8e89c37f8607060cb0cf
David Parushev <davidn.1107@gmail.com>
2019-01-18 13:55:08 +0200

    Partical system

--- Assets/AccelerometerReaction.cs
+++ Assets/AccelerometerReaction.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,26 +7,6 @@ public class AccelerometerReaction : MonoBehaviour
     public Rigidbody body;
     public Text UI;
     public float multiplier = 1.0f;
-
-    //    public void Update()
-    //    {
-    //        var accel = Vector3.zero;
-
-    //        //if (Application.platform == RuntimePlatform.WindowsEditor)
-    //        {
-    //            accel = Input.mousePosition;
-    //        }
-    //        // if (Application.platform == RuntimePlatform.Android)
-    //        {
-    //            accel = Input.acceleration;
-    //        }
-
-    //        body.velocity += accel;
-    //        if (UI != null)
-    //        {
-    //            UI.text = string.Format("acceleration: {0}\nvelocity: {1}", accel, body.velocity);
-    //        }
-    //    }
 
     float accelerometerUpdateInterval = 1.0f / 60.0f;
     // The greater the value of LowPassKernelWidthInSeconds, the slower the
@@ -39,7 +18,6 @@ public class AccelerometerReaction : MonoBehaviour
 
     float lowPassFilterFactor;
     Vector3 lowPassValue;
-
 
     void Start()
     {
